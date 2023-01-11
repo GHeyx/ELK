68eff93974fcd7a0ac3f771be2d0d1b38fc125f8
GHeyx <54897048+GHeyx@users.noreply.github.com>
2023-01-11 14:27:34 -0500

    Add files via upload

--- person.py
+++ person.py
@@ -1,8 +1,13 @@
-#This is a Person class that will be used to create a person object for a person's card in a family tree application
+from tkinter import *
+# This is a Person class that will be used to create a person object for a person's card in a family tree application
+# The person class will be required to have the following attributes: first name
+
+# The following atttributes will be optional:
 class Person:
-    def __init__(self, name, age, dob, pob, occupation):
-        self.name = name
-        self.age = age
+    def __init__(self, firstName, lastName, gender, dob, pob, occupation):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.gender = gender
         self.dob = dob
         self.pob = pob
         self.occupation = occupation
@@ -12,3 +17,4 @@ class Person:
 
     def __str__(self):
         return "Person: {} is"
+
